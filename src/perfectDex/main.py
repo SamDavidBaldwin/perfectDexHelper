9fe5ce3124f95dd7370da7aa42e6aa6cb1963b76
Samuel Baldwin <sdb035@bucknell.edu>
2024-06-04 21:50:49 -0400

    Adding dex 01

--- src/perfectDex/main.py
+++ src/perfectDex/main.py
@@ -1,3 +1,16 @@
+import os 
+import json
 
 if __name__ == "__main__":
   print("Default")
+  home = os.path.abspath(os.getcwd())
+  dex = home + "\src\lib\dex"
+
+
+  for file in os.listdir(dex):    
+    with open(dex + "\\" + file, "r") as f: 
+      data = json.load(f)
+      for i in data: 
+        print(i,   data[i])
+      
+      f.close()
