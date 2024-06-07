0434c4dfafe3419a958d25b02ece706ffc07dfa8
Samuel Baldwin <sdb035@bucknell.edu>
2024-06-06 23:42:20 -0400

    Adding basic options

--- src/perfectDex/main.py
+++ src/perfectDex/main.py
@@ -1,5 +1,6 @@
 import os 
 import json
+from perfectDex import perfectDex
 
 def createJSONForDex(list):
   with open(dex + "\\" + list[0] + "_" + list[1].lower() + ".json", "w", encoding = 'utf-8') as f:
@@ -14,7 +15,7 @@ def createJSONForDex(list):
       output["tasks"][element[0]] = [0,  element[1]]
     json.dump(output, f)
 
-def readInTasks(tasks):
+def readInTasks():
   for file in os.listdir(tasks):   
     with open(tasks + "\\" + file, "r") as f:
      #Kill the first line 
@@ -33,26 +34,94 @@ def readInTasks(tasks):
             num = out[i].split("(")[1][0:-1]
             out[i] = [out[i].split(" (")[0], num]
         createJSONForDex(out)     
-            
-if __name__ == "__main__":
-  print("Default")
-  home = os.path.abspath(os.getcwd())
-  dex = home + "\src\lib\dex"
-  tasks = home + "\src\lib\\tasks"
 
+def resetStatus():
   #Idea for reset
   # if (input("Would you like to reset")) == "T":
   #   for file in os.listdir(dex):
   #    if os.path.exists(dex + "\\" + file):
   #     os.remove(dex + "\\" + file)
-      
-  
+  return
 
+def createProfile():
+  location = home + "\\src\\lib\\user\\"
+  print(location)
+  while True: 
+    name = input("Provide a user name")
+    if os.path.exists(location+name):
+       pass
+    else:
+      os.mkdir(location+name)
+      break
+
+def loadProfile():
+  return
+
+def removeProfile():
+  return
+
+def updateProfileManual():
+  return
+
+def updateProfileAutomatic():
+  return
+
+def monitor():
+  return
+
+def suggest(): 
+  sugg = perfectDex()
+  sugg.testMethod()
+
+if __name__ == "__main__":
+  print("Default")
+  home = os.path.abspath(os.getcwd())
+  dex = home + "\src\lib\dex"
+  tasks = home + "\src\lib\\tasks"
+  exit = True
+
+  options = [
+    "<--------------COMMANDS ------------->",
+    "1: Init",
+    "2: Reset",
+    "3: New Profile",
+    "4: Load Profile",
+    "5: Remove Profile",
+    "6: Update Profile (Manual)",
+    "7: Update Profile (Automatic)",
+    "8: Monitor Screen for Updates",
+    "9: Suggestions",
+    "0: Quit", 
+    "--------------------------------------"
+  ]
+
+  func = {
+    "1": readInTasks,
+    "2": resetStatus,
+    "3": createProfile,
+    "4": loadProfile,
+    "5": removeProfile,
+    "6": updateProfileManual,
+    "7": updateProfileManual,
+    "8": monitor,
+    "9": suggest,
+  }
+
+  while exit == True:
+    print("\n".join(options))
+    command = input("Command: ")
+    if command == "0":
+      exit = False
+      break
+    if command in func:
+      func[command]()
+    else: 
+      print("Command not found")
+    
+    
+    
   #Reading in the master tasks file to create individual CSV's for each dex entry
-  readInTasks(tasks)   
-      
       #Create the CSV file in the dex
-
   #Read the created CSV files for the remaining tasks
   #for file in os.listdir(dex):    
     #with open(dex + "\\" + file, "r") as f: 
--- src/perfectDex/perfectDex.py
+++ src/perfectDex/perfectDex.py
@@ -2,3 +2,7 @@ class perfectDex():
 
   def main(self):
     #Read through existing Dex File
+    print("WAHOO")
+
+  def testMethod(self):
+    print("test String")
